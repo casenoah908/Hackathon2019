9fb0ae4f90ad810908747c2e44de24c12c77716b
Noah Schottler <noahschottler1@gmail.com>
2019-10-05 14:50:23 -0500

    Added schottler did this

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //wassup
         //My wassup
+        //Schottler did this
     }
 }
