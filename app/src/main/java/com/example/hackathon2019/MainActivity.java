10c0d8b75f38feebedd5853ba41a18ae54b44258
Trevor Schmidt <trevor.r.schmidt@ndus.edu>
2019-10-05 14:43:54 -0500

    did a thing again

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //wassup
         //My wassup
-        //yo yo
+        //something else
     }
 }
