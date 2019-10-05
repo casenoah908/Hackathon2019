1284f084f5b6651ecc2172c1d6a5575b65297618
Noah Case <casenoah908@gmail.com>
2019-10-05 15:24:40 -0500

    Added an instance of retrofit,

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
         //wassup
         //My wassup
         //Schottler did this
+
+
+
+        //RETROFIT instantiation
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl( "server url")
+                .addConverterFactory(GsonConverterFactory.create());
+        //interface methods add to the base url with their relative urls
+        Retrofit retrofit = builder.build(); //build whats defined above
+        API client = retrofit.create(API.class);
+        //create call
+        Call<UserAccount> call = client.account();
+        //end RETROFIT Instantiation
     }
 }
