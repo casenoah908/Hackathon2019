19a181ec135446aab1f1a8a9a115618936598d22
Noah Case <casenoah908@gmail.com>
2019-10-06 00:17:17 -0500

    finished thread activity

--- app/src/main/java/com/example/hackathon2019/ThreadActivity.java
+++ app/src/main/java/com/example/hackathon2019/ThreadActivity.java
@@ -15,8 +15,15 @@ public class ThreadActivity extends AppCompatActivity {
         Thread thread = (Thread) getIntent().getExtras().getSerializable("thread");
 
 
+        //instantiate views
         TextView title = findViewById(R.id.titleTextView);
+        TextView userAndDate = findViewById(R.id.userAndDateTextView);
+        TextView description = findViewById(R.id.descriptionTextView);
+
         //dimensionThreads[position].getType()+" - "+dimensionThreads[position].getTitle()
         title.setText(thread.getType()+" - "+thread.getTitle());
+        userAndDate.setText(thread.getUserName()+" "+thread.getUserDays()+" days - "+thread.getDateTime());
+        description.setText(thread.getDescription());
+
     }
 }
